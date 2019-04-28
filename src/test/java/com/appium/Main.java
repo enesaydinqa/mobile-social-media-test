053cc375b9987454abf8b2037762640c57e11a7a
Enes Aydın <enes.aydin@sahibinden.com>
2019-04-28 16:22:30 +0300

    update

--- src/test/java/com/appium/Main.java
+++ src/test/java/com/appium/Main.java
@@ -4,17 +4,24 @@ import com.appium.mobile.test.instagram.InstagramMultipleDeviceTest;
 import com.appium.mobile.test.instagram.InstagramSingleDeviceTest;
 import org.junit.runner.JUnitCore;
 
-public class Main {
-    public static void main(String[] args) throws Exception {
+public class Main
+{
+    public static void main(String[] args) throws Exception
+    {
 
         String testType = System.getProperties().getProperty("test.app.prop");
         Boolean multipleDeviceTest = Boolean.getBoolean(System.getProperties().getProperty("multiple.device.test"));
 
-        switch (testType) {
+        switch (testType)
+        {
             case "INSTAGRAM":
-                if (multipleDeviceTest) {
+
+                if (multipleDeviceTest)
+                {
                     JUnitCore.runClasses(InstagramSingleDeviceTest.class, InstagramMultipleDeviceTest.class);
-                } else if (!multipleDeviceTest) {
+                }
+                else if (!multipleDeviceTest)
+                {
                     JUnitCore.runClasses(InstagramSingleDeviceTest.class);
                 }
                 break;
